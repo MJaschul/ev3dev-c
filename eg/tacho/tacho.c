fb07a1e87ca0e68a09124071dda6a5777687576e
MJay <m.jaschul@gmail.com>
2021-09-13 20:17:44 +0200

    Make Tacho example work

--- eg/tacho/tacho.c
+++ eg/tacho/tacho.c
@@ -3,7 +3,8 @@
     | |_  \ \  /   ) ) | |  ) | |_  \ \  /  \(@)- /
     |_|__  \_\/  __)_) |_|_/  |_|__  \_\/   /(@)- \
                                                ((())))
- *//**
+ */
+/**
  *  \file  tacho.c
  *  \brief  ev3dev-c Tacho Motors example.
  *  \author  Vitaly Kravtsov (in4lio@gmail.com)
@@ -24,74 +25,91 @@
 #else
 
 #include <unistd.h>
-#define Sleep( msec ) usleep(( msec ) * 1000 )
+#define Sleep(msec) usleep((msec)*1000)
 
 //////////////////////////////////////////////////
 #endif
 
-int main( void )
+int main(void)
 {
 	int i;
 	uint8_t sn;
 	FLAGS_T state;
-	char s[ 256 ];
+	char s[256];
 
 #ifndef __ARM_ARCH_4T__
 	/* Disable auto-detection of the brick (you have to set the correct address below) */
 	ev3_brick_addr = "192.168.0.204";
 
 #endif
-	if ( ev3_init() == -1 ) return ( 1 );
+	if (ev3_init() == -1)
+		return (1);
 
 #ifndef __ARM_ARCH_4T__
-	printf( "The EV3 brick auto-detection is DISABLED,\nwaiting %s online with plugged tacho...\n", ev3_brick_addr );
+	printf("The EV3 brick auto-detection is DISABLED,\nwaiting %s online with plugged tacho...\n", ev3_brick_addr);
 
 #else
-	printf( "Waiting tacho is plugged...\n" );
+	printf("Waiting tacho is plugged...\n");
 
 #endif
-	while ( ev3_tacho_init() < 1 ) Sleep( 1000 );
+	while (ev3_tacho_init() < 1)
+		Sleep(1000);
 
-	printf( "*** ( EV3 ) Hello! ***\n" );
+	printf("*** ( EV3 ) Hello! ***\n");
 
-	printf( "Found tacho motors:\n" );
-	for ( i = 0; i < DESC_LIMIT; i++ ) {
-		if ( ev3_tacho[ i ].type_inx != TACHO_TYPE__NONE_ ) {
-			printf( "  type = %s\n", ev3_tacho_type( ev3_tacho[ i ].type_inx ));
-			printf( "  port = %s\n", ev3_tacho_port_name( i, s ));
+	printf("Found tacho motors:\n");
+	for (i = 0; i < DESC_LIMIT; i++)
+	{
+		if (ev3_tacho[i].type_inx != TACHO_TYPE__NONE_)
+		{
+			printf("  type = %s\n", ev3_tacho_type(ev3_tacho[i].type_inx));
+			printf("  port = %s\n", ev3_tacho_port_name(i, s));
 		}
 	}
-	if ( ev3_search_tacho( LEGO_EV3_M_MOTOR, &sn, 0 )) {
+	if (ev3_search_tacho(LEGO_EV3_L_MOTOR, &sn, 0))
+	{
 		int max_speed;
+		int position;
 
-		printf( "LEGO_EV3_M_MOTOR is found, run for 5 sec...\n" );
-		get_tacho_max_speed( sn, &max_speed );
-		printf("  max_speed = %d\n", max_speed );
-		set_tacho_stop_action_inx( sn, TACHO_COAST );
-		set_tacho_speed_sp( sn, max_speed * 2 / 3 );
-		set_tacho_time_sp( sn, 5000 );
-		set_tacho_ramp_up_sp( sn, 2000 );
-		set_tacho_ramp_down_sp( sn, 2000 );
-		set_tacho_command_inx( sn, TACHO_RUN_TIMED );
+		printf("LEGO_EV3_L_MOTOR is found, run for 5 sec...\n");
+		get_tacho_max_speed(sn, &max_speed);
+		printf("  max_speed = %d\n", max_speed);
+		set_tacho_stop_action_inx(sn, TACHO_COAST);
+		set_tacho_speed_sp(sn, max_speed * 2 / 3);
+		set_tacho_time_sp(sn, 5000);
+		set_tacho_ramp_up_sp(sn, 2000);
+		set_tacho_ramp_down_sp(sn, 2000);
+		set_tacho_command_inx(sn, TACHO_RUN_TIMED);
 		/* Wait tacho stop */
-		Sleep( 100 );
-		do {
-			get_tacho_state_flags( sn, &state );
-		} while ( state );
-		printf( "run to relative position...\n" );
-		set_tacho_speed_sp( sn, max_speed / 2 );
-		set_tacho_ramp_up_sp( sn, 0 );
-		set_tacho_ramp_down_sp( sn, 0 );
-		set_tacho_position_sp( sn, 90 );
-		for ( i = 0; i < 8; i++ ) {
-			set_tacho_command_inx( sn, TACHO_RUN_TO_REL_POS );
-			Sleep( 500 );
+		Sleep(100);
+		do
+		{
+			get_tacho_state_flags(sn, &state);
+		} while (state);
+		printf("run to relative position...\n");
+		set_tacho_speed_sp(sn, max_speed / 2);
+		set_tacho_ramp_up_sp(sn, 0);
+		set_tacho_ramp_down_sp(sn, 0);
+		set_tacho_position_sp(sn, 90);
+		for (i = 0; i < 8; i++)
+		{
+			set_tacho_command_inx(sn, TACHO_RUN_TO_ABS_POS);
+			/* Wait tacho stop */
+			Sleep(100);
+			do
+			{
+				get_tacho_state_flags(sn, &state);
+			} while (state);
+			get_tacho_position(sn, &position);
+			printf("get_tacho_position: %d\n", position);
 		}
-	} else {
-		printf( "LEGO_EV3_M_MOTOR is NOT found\n" );
+	}
+	else
+	{
+		printf("LEGO_EV3_L_MOTOR is NOT found\n");
 	}
 	ev3_uninit();
-	printf( "*** ( EV3 ) Bye! ***\n" );
+	printf("*** ( EV3 ) Bye! ***\n");
 
-	return ( 0 );
+	return (0);
 }
